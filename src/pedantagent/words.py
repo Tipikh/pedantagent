836b01ade228988cd49525312ef37b5bfabe959d
Tipikh <thibautp09@hotmail.com>
2026-01-01 15:34:08 +0100

    Add more words

--- src/pedantagent/words.py
+++ src/pedantagent/words.py
@@ -1,8 +1,15 @@
 def warmup_words() -> list[str]:
     # Démarrage simple : mots fréquents pour révéler la structure
     return [
-        "de", "la", "le", "les", "un", "une", "des", "et", "ou",
-        "est", "sont", "être", "avoir", "dans", "sur", "avec",
+
+        "de", "la", "le", "les", "un", "une", "des", "et", "ou", "l", "d"
+        "guerre", "travail", "animal", "politique", "pays", "europe", "asie", "amérique"
+        "santé", "science", "empereur", "histoire",
+        "1950", "mort", "homme", "est", "sont", "est", "avoir", "dans", "sur", "avec",
         "par", "pour", "au", "aux", "du", "ce", "cette",
         "qui", "que", "dont", "plus", "moins", "entre",
+        "Nord", "Sud", "Ouest", "plus", "moins", "entre", "physique", "psychologie",
+        "entreprise", "argent", "économie", "musique", "cinéma", "écriture", "art", "poisson",
+        "montagne", "mer", "océan", "énergie", "charbon", "chimie"
+        
     ]
