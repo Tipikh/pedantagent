e636e393ef4853f0ea1cb8c6662f659505b7e78e
Tipikh <thibautp09@hotmail.com>
2026-01-06 19:17:32 +0100

    Add some words

--- src/pedantagent/words.py
+++ src/pedantagent/words.py
@@ -2,14 +2,18 @@ def warmup_words() -> list[str]:
     # Démarrage simple : mots fréquents pour révéler la structure
     return [
 
-        "de", "la", "le", "les", "un", "une", "des", "et", "ou", "l", "d"
-        "guerre", "travail", "animal", "politique", "pays", "europe", "asie", "amérique"
+        "de", "la", "le", "les", "un", "une", "des", "et", "ou", "l", "d",
+        "guerre", "travail", "animal", "politique", "pays", "europe", "asie", "amérique",
         "santé", "science", "empereur", "histoire",
         "1950", "mort", "homme", "est", "sont", "est", "avoir", "dans", "sur", "avec",
         "par", "pour", "au", "aux", "du", "ce", "cette",
         "qui", "que", "dont", "plus", "moins", "entre",
-        "Nord", "Sud", "Ouest", "plus", "moins", "entre", "physique", "psychologie",
-        "entreprise", "argent", "économie", "musique", "cinéma", "écriture", "art", "poisson",
-        "montagne", "mer", "océan", "énergie", "charbon", "chimie"
+        "Nord", "Sud", "Ouest", "physique", "psychologie",
+        "entreprise", "argent", "économie", "musique", "cinéma", "écriture", "poisson",
+        "montagne", "mer", "océan", "énergie", "charbon", "chimie", "1950", "2014", "1523",
+        "école", "examen", "maladie", "biologie", "1", "5", "V", "II", "I", "III", "XI",
+        "art", "poésie", "littérature", "philosophie", "France", "Chine", "Japon", "Palestine",
+        "Afrique", "Orient", "Arme", "Roi", "président", "er", "Autriche", "Canada", "Argentine",
+        "bateau", "véhicule", "feu"
         
     ]
